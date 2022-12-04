fd17b2642629ee908acbb567bb7b7fd9f32cf5eb
Melissa Selk <selk.melissa@gmail.com>
2022-12-03 18:14:39 -0800

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+const isProduction = process.env.NODE_ENV === 'production'
+
 const nextConfig = {
 	images: {
 		unoptimized: true,
 	},
-	assetPrefix: '/mselk',
+	assetPrefix: isProduction ? '/mselk' : '',
 };
 
 export default nextConfig;
