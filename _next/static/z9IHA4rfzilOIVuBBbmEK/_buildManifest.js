f2103bc65e99adff270daea066a8b5b2b0237432
mselk <119761394+mselk@users.noreply.github.com>
2022-12-04 01:51:31 +0000

    Deploying to public from @ mselk/mselk@d7edc6fc73e1bd3093295e277b50a467baa1b230 🚀

---
+++ _next/static/z9IHA4rfzilOIVuBBbmEK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f191900b94351358.css","static/chunks/pages/index-0b48933d80755a4a.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
