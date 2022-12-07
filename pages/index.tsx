1886f0a445d10a13d8563bb29b75ae0801e84e1b
Melissa Selk <selk.melissa@gmail.com>
2022-12-06 21:03:18 -0800

    added a city

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,17 @@ import styles from '../styles/Home.module.css';
 import melissa from '../public/melissa.jpg';
 import Tile from '../components/tile/tile';
 import tiles from '../data/tiles';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+	const [city, setCity] = useState("");
+	useEffect(() => {
+		fetch("https://ipinfo.io/geo?token=7abe1afad1fdd6").then(response => {
+			response.json().then(data => {
+				setCity(data.city);
+			})
+		})
+	}, [])
 	return (
 		<div className={styles.container}>
 			<Head>
@@ -14,6 +23,7 @@ export default function Home() {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main>
+				Thank you for visiting. Your city: {city}
 				<h1 className={styles.headline}>Welcome to my Blog</h1>
 				<p> This was my adventure in the Caribbean.</p>
 				<Image alt="woman on a beach" src={melissa} width={1086} height={1085} />
